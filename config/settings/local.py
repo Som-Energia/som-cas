3dec48050908eedf39e503e03b0c3c6c463c5516
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2018-03-01 19:08:08 +0100

    Initial commit

---
+++ config/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+
+MAMA_CAS_SERVICES = [
+    {
+        'SERVICE': '^https://participa\.somenergia\.coop[\]?.*',
+        'CALLBACKS': [
+            'som_cas.callbacks.participa',
+        ],
+        'LOGOUT_ALLOW': True,
+        'LOGOUT_URL': 'https://participa.somenergia.coop/logout',
+        'PROXY_ALLOW': True,
+        'PROXY_PATTERN': '^https://participa\.somenergia\.coop',
+    }
+
+]
+
+MAMA_CAS_SERVICE_BACKENDS = [
+    'mama_cas.services.backends.SettingsBackend'
+]
---
+++ som_cas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SomCasConfig(AppConfig):
+    name = 'som_cas'
