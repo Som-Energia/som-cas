efc127263d47a86cb8c8b21ad2d328181a0645d9
Juan Pedro Sánchez <juanpe.sn@gmail.com>
2020-05-25 13:32:53 +0200

    Clean unused imports

--- som_cas/views.py
+++ som_cas/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-
 from mama_cas.views import LoginView
 
 from .forms import SomCasLoginForm
 
-logger = logging.getLogger(__name__)
 
 class SomCasLoginView(LoginView):
 
