52a189b525be1cd99e51251b6680b86001b5c0fa
Marite Guerrieri <marite.guerrieri@somenergia.coop>
2025-07-01 11:52:53 +0200

    ✨ new usuaris backened

--- som_cas/backends.py
+++ som_cas/backends.py
@@ -16,6 +16,65 @@ UserModel = get_user_model()
 class RegisterException(Exception):
     pass
 
+class UsuarisBackend(object):
+    """
+    Backend athentication for SomEnergia users
+    for more details of the implementation: https://docs.djangoproject.com/en/2.2/topics/auth/customizing/
+    """
+
+    BASE_QUERY_SOCIS = 'select * from som_users where {conditions};'
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        socis_by_username = self.BASE_QUERY_SOCIS.format(
+            conditions='username = \'{}\''
+        )
+        try:
+            user = self._fetch_user_from_db(
+                socis_by_username.format(username.upper())
+            )
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)
+            return None
+        else:
+            if not check_password(password, user.password):
+                return None
+
+            msg = "Request authentication %s@%s"
+            logger.debug(msg, username, request.GET.get('service'))
+            user.save()
+            if is_assembly_service(request.GET.get('service', '')):
+                registry = user.register_in_virtual_assembly()
+                if registry is None:
+                    raise RegisterException()
+            return user
+
+    def get_user(self, user_id):
+        try:
+            user = UserModel.objects.get(id=user_id)
+        except UserModel.DoesNotExist:
+            return None
+        else:
+            return user
+
+    def _fetch_user_from_db(self, user_query):
+        """
+        Fetch a user from database defined in 'users_db' DATABASES settings.
+        """
+        try:
+            with connections['users_db'].cursor() as cursor:
+                cursor.execute(user_query)
+                columns = [col.name for col in cursor.description]
+                user = cursor.fetchone()
+        except Exception as e:
+            msg = "An error occured executing '%s': %s"
+            logger.error(msg, user_query, e)
+            raise e
+        else:
+            if user:
+                raw_user = dict(zip(columns, user))
+                return UserModel(**raw_user)
+            else:
+                raise UserModel.DoesNotExist()
 
 class SocisBackend(object):
     """
